96471735f1f56418c73ddd7fd87276f856d166b6
Saul Malagon Martinez <sark_blues_friki95@hotmail.com>
2025-05-08 21:29:49 -0600

    1st commit

---
+++ src/main/java/io/github/saulmmbp/BackendVcardApplication.java
@@ -0,0 +1,13 @@
+package io.github.saulmmbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendVcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendVcardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/saulmmbp/BackendVcardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.saulmmbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendVcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
