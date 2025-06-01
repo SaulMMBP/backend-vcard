62124183ca963494f05539b41c9310210a3beda7
Saul Malagon Martinez <sark_blues_friki95@hotmail.com>
2025-05-31 20:13:30 -0600

    Se agrega manejador de errores

---
+++ src/main/java/io/github/saulmmbp/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package io.github.saulmmbp.exceptions;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import io.github.saulmmbp.dtos.ErrorDetailsDto;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(RuntimeException.class)
+	public ResponseEntity<ErrorDetailsDto> runtimeExceptionHandler(RuntimeException exception, WebRequest request) {
+		ErrorDetailsDto errorDetails = new ErrorDetailsDto(new Date(), exception.toString(), request.getDescription(false));
+		return ResponseEntity.internalServerError().body(errorDetails);
+	}
+	
+	@ExceptionHandler(ApplicationException.class)
+	public ResponseEntity<ErrorDetailsDto> applicationExceptionHandler(ApplicationException exception, WebRequest request) {
+		ErrorDetailsDto errorDetails = new ErrorDetailsDto(new Date(), exception.getMessage(), request.getDescription(false));
+		return ResponseEntity.internalServerError().body(errorDetails);
+	}
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<ErrorDetailsDto> resourceNotFoundExceptionHandler(ResourceNotFoundException exception, WebRequest request) {
+		ErrorDetailsDto errorDetails = new ErrorDetailsDto(new Date(), exception.getMessage(), request.getDescription(false));
+		return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(ResourceNotBelongsUserException.class)
+	public ResponseEntity<ErrorDetailsDto> resourceNotBelongsUserExceptionHandler(ResourceNotBelongsUserException exception, WebRequest request) {
+		ErrorDetailsDto errorDetails = new ErrorDetailsDto(new Date(), exception.getMessage(), request.getDescription(false));
+		return new ResponseEntity<>(errorDetails, HttpStatus.UNAUTHORIZED);
+	}
+}
