cd5acba901e192b472a45e99ea10bca8c1df1f92
Saul Malagon Martinez <sark_blues_friki95@hotmail.com>
2025-05-31 20:15:14 -0600

    Se agregan representation Models para response RestFul

---
+++ src/main/java/io/github/saulmmbp/representationmodels/ContactModel.java
@@ -0,0 +1,30 @@
+package io.github.saulmmbp.representationmodels;
+
+import java.util.List;
+
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@JsonInclude(Include.NON_NULL)
+@Relation(collectionRelation = "contacts")
+public class ContactModel extends RepresentationModel<ContactModel> {
+
+	private Long id;
+	private String userId;
+	private String identifier;
+	private String name;
+	private String email;
+	private String position;
+	private String company;
+	private String web;
+	private List<String> phones;
+	
+}
---
+++ src/main/java/io/github/saulmmbp/representationmodels/VCardModel.java
@@ -0,0 +1,20 @@
+package io.github.saulmmbp.representationmodels;
+
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
+
+import io.github.saulmmbp.dtos.ContactResponseDto;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Relation(collectionRelation = "vcards")
+public class VCardModel extends RepresentationModel<VCardModel> {
+
+	private String name;
+	private String color;
+	private ContactResponseDto contact;
+	private byte[] qr;
+	
+}
