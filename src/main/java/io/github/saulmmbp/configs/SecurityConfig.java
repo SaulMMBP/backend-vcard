1658609ead9de6d91b6277079cbfe1daa2f496d3
Saul Malagon Martinez <sark_blues_friki95@hotmail.com>
2025-06-01 19:04:49 -0600

    Se cambió el tipo de autenticacion para aws lambda

--- src/main/java/io/github/saulmmbp/configs/SecurityConfig.java
+++ src/main/java/io/github/saulmmbp/configs/SecurityConfig.java
@@ -1,13 +1,17 @@
 package io.github.saulmmbp.configs;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,15 +19,36 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-	@Bean
-	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		return http
-				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.authorizeHttpRequests(req -> req.anyRequest().authenticated())
-				.oauth2ResourceServer(customizer -> customizer.jwt(Customizer.withDefaults()))
-				.cors(Customizer.withDefaults())
-				.csrf(CsrfConfigurer::disable)
-				.build();
-	}
-	
+    @Value("${spring.security.basic.username}")
+    private String basicUsername;
+
+    @Value("${spring.security.basic.password}")
+    private String basicPassword;
+
+    private String basicRole = "LAMBDA";
+
+    @Bean
+    @Order(1)
+    SecurityFilterChain basicSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http.securityMatchers(matchers -> matchers.requestMatchers(HttpMethod.POST, "/users/**"))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(req -> req.anyRequest().authenticated()).httpBasic(Customizer.withDefaults())
+                .cors(Customizer.withDefaults()).csrf(CsrfConfigurer::disable).build();
+    }
+
+    @Bean
+    @Order(2)
+    SecurityFilterChain cognitoSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(req -> req.anyRequest().authenticated())
+                .oauth2ResourceServer(customizer -> customizer.jwt(Customizer.withDefaults()))
+                .cors(Customizer.withDefaults()).csrf(CsrfConfigurer::disable).build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername(basicUsername).password(basicPassword).roles(basicRole).build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
 }
--- src/main/java/io/github/saulmmbp/controllers/UserController.java
+++ src/main/java/io/github/saulmmbp/controllers/UserController.java
@@ -1,34 +1,28 @@
 package io.github.saulmmbp.controllers;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
 
 import io.github.saulmmbp.dtos.UserRequestDto;
 import io.github.saulmmbp.services.IUserService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("users")
 @AllArgsConstructor
+@Slf4j
 public class UserController {
 
-	private IUserService userService;
-	
-	@PostMapping
-	@PreAuthorize("hasAuthority('SCOPE_backend-vcard/users/create')")
-	public ResponseEntity<?> createUser(@Valid @RequestBody UserRequestDto userRequest) {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		System.out.println("Authorities: " + auth.getAuthorities());
-		userService.createUser(userRequest);
-		return new ResponseEntity<>(HttpStatus.CREATED);
-	}
-	
+    private IUserService userService;
+
+    @PostMapping
+    public ResponseEntity<?> createUser(@Valid @RequestBody UserRequestDto userRequest) {
+        log.info("INICIA CONTROLADOR POST users");
+        userService.createUser(userRequest);
+        log.info("TERMINA CONTROLADOR POST users");
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
 }
