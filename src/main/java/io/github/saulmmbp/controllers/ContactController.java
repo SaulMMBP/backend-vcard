42a744f14123537055a1e846cbb5cba4b11d27e0
Saul Malagon Martinez <sark_blues_friki95@hotmail.com>
2025-06-01 19:05:19 -0600

    Se agregó logging

--- src/main/java/io/github/saulmmbp/controllers/ContactController.java
+++ src/main/java/io/github/saulmmbp/controllers/ContactController.java
@@ -24,46 +24,62 @@ import io.github.saulmmbp.services.IContactService;
 import io.github.saulmmbp.utils.assemblers.ContactModelAssembler;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("{userId}/contacts")
 @AllArgsConstructor
 @PreAuthorize("#userId == authentication.principal.subject")
+@Slf4j
 public class ContactController {
 
-	private IContactService contactService;
-	private ContactModelAssembler contactModelAssembler;
-	private PagedResourcesAssembler<ContactResponseDto> contactPagedAssembler;
-	
-	@GetMapping
-	public ResponseEntity<PagedModel<ContactModel>> getContactsByUserId(@PathVariable String userId, @PageableDefault Pageable pageable) {
-		Page<ContactResponseDto> contacts = contactService.getContactsByUserId(userId, pageable);
-		PagedModel<ContactModel> pagedModel = contactPagedAssembler.toModel(contacts, contactModelAssembler);
-		return ResponseEntity.ok(pagedModel);
-	}
-	
-	@GetMapping("{contactId}")
-	public ResponseEntity<ContactResponseDto> getContactByIdAndUserId(@PathVariable Long contactId, @PathVariable String userId) {
-		ContactResponseDto contact = contactService.getContactByIdAndUserId(contactId, userId);
-		return ResponseEntity.ok(contact);
-	}
-	
-	@PostMapping
-	public ResponseEntity<ContactResponseDto> createContactForUser(@PathVariable String userId, @Valid @RequestBody ContactRequestDto contactRequest) {
-		ContactResponseDto contact = contactService.createContactForUser(userId, contactRequest);
-		return new ResponseEntity<>(contact, HttpStatus.CREATED);
-	}
-	
-	@PutMapping("{contactId}")
-	public ResponseEntity<ContactResponseDto> updateContactForUser(@PathVariable String userId, @PathVariable Long contactId, @Valid @RequestBody ContactRequestDto contactRequest) {
-		ContactResponseDto contact = contactService.updateContactForUser(contactId, userId, contactRequest);
-		return ResponseEntity.ok(contact);
-	}
-	
-	@DeleteMapping("{contactId}")
-	public ResponseEntity<?> deleteContactForUser(@PathVariable String userId, @PathVariable Long contactId) {
-		contactService.deleteContactForUser(contactId, userId);
-		return ResponseEntity.noContent().build();
-	}
-	
+    private IContactService contactService;
+    private ContactModelAssembler contactModelAssembler;
+    private PagedResourcesAssembler<ContactResponseDto> contactPagedAssembler;
+
+    @GetMapping
+    public ResponseEntity<PagedModel<ContactModel>> getContactsByUserId(@PathVariable String userId,
+            @PageableDefault Pageable pageable) {
+        log.info("INICIA CONTROLADOR GET {}/contacts/", userId);
+        Page<ContactResponseDto> contacts = contactService.getContactsByUserId(userId, pageable);
+        PagedModel<ContactModel> pagedModel = contactPagedAssembler.toModel(contacts, contactModelAssembler);
+        log.info("TERMINA CONTROLADOR GET {}/contacts/", userId);
+        return ResponseEntity.ok(pagedModel);
+    }
+
+    @GetMapping("{contactId}")
+    public ResponseEntity<ContactResponseDto> getContactByIdAndUserId(@PathVariable Long contactId,
+            @PathVariable String userId) {
+        log.info("INICIA CONTROLADOR GET {}/contacts/{}", userId, contactId);
+        ContactResponseDto contact = contactService.getContactByIdAndUserId(contactId, userId);
+        log.info("TERMINA CONTROLADOR GET {}/contacts/{}", userId, contactId);
+        return ResponseEntity.ok(contact);
+    }
+
+    @PostMapping
+    public ResponseEntity<ContactResponseDto> createContactForUser(@PathVariable String userId,
+            @Valid @RequestBody ContactRequestDto contactRequest) {
+        log.info("INICIA CONTROLADOR POST {}/contacts/", userId);
+        ContactResponseDto contact = contactService.createContactForUser(userId, contactRequest);
+        log.info("TERMINA CONTROLADOR POST {}/contacts/", userId);
+        return new ResponseEntity<>(contact, HttpStatus.CREATED);
+    }
+
+    @PutMapping("{contactId}")
+    public ResponseEntity<ContactResponseDto> updateContactForUser(@PathVariable String userId,
+            @PathVariable Long contactId, @Valid @RequestBody ContactRequestDto contactRequest) {
+        log.info("INICIA CONTROLADOR PUT {}/contacts/{}", userId, contactId);
+        ContactResponseDto contact = contactService.updateContactForUser(contactId, userId, contactRequest);
+        log.info("TERMINA CONTROLADOR PUT {}/contacts/{}", userId, contactId);
+        return ResponseEntity.ok(contact);
+    }
+
+    @DeleteMapping("{contactId}")
+    public ResponseEntity<?> deleteContactForUser(@PathVariable String userId, @PathVariable Long contactId) {
+        log.info("INICIA CONTROLADOR DELETE {}/contacts/{}", userId, contactId);
+        contactService.deleteContactForUser(contactId, userId);
+        log.info("TERMINA CONTROLADOR DELETE {}/contacts/{}", userId, contactId);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/io/github/saulmmbp/controllers/UserSummaryController.java
+++ src/main/java/io/github/saulmmbp/controllers/UserSummaryController.java
@@ -2,27 +2,28 @@ package io.github.saulmmbp.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import io.github.saulmmbp.dtos.SummaryResponseDto;
 import io.github.saulmmbp.services.IUserSummaryService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("{userId}/summary")
 @AllArgsConstructor
 @PreAuthorize("#userId == authentication.principal.subject")
+@Slf4j
 public class UserSummaryController {
 
-	private IUserSummaryService userSummaryService;
-	
-	@GetMapping
-	public ResponseEntity<SummaryResponseDto> getGeneralInformationByUserId(@PathVariable String userId) {
-		SummaryResponseDto generalInformation = userSummaryService.getSummaryByUserId(userId);
-		return ResponseEntity.ok(generalInformation);
-	}
-	
+    private IUserSummaryService userSummaryService;
+
+    @GetMapping
+    public ResponseEntity<SummaryResponseDto> getGeneralInformationByUserId(@PathVariable String userId) {
+        log.info("INICIA CONTROLADOR GET {}/summary/", userId);
+        SummaryResponseDto generalInformation = userSummaryService.getSummaryByUserId(userId);
+        log.info("TERMINA CONTROLADOR GET {}/summary/", userId);
+        return ResponseEntity.ok(generalInformation);
+    }
+
 }
