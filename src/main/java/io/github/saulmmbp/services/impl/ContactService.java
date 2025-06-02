203e8bcdadbc11b63c60f917c846e6b29b5fb5f7
Saul Malagon Martinez <sark_blues_friki95@hotmail.com>
2025-06-01 19:06:28 -0600

    Se agrega validación de existencia de usuario al crear recurso

--- src/main/java/io/github/saulmmbp/services/impl/ContactService.java
+++ src/main/java/io/github/saulmmbp/services/impl/ContactService.java
@@ -1,19 +1,14 @@
 package io.github.saulmmbp.services.impl;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 
-import io.github.saulmmbp.dtos.ContactRequestDto;
-import io.github.saulmmbp.dtos.ContactResponseDto;
-import io.github.saulmmbp.entities.Contact;
-import io.github.saulmmbp.entities.Phone;
-import io.github.saulmmbp.exceptions.ResourceNotBelongsUserException;
-import io.github.saulmmbp.exceptions.ResourceNotFoundException;
-import io.github.saulmmbp.repositories.IContactRepository;
+import io.github.saulmmbp.dtos.*;
+import io.github.saulmmbp.entities.*;
+import io.github.saulmmbp.exceptions.*;
+import io.github.saulmmbp.repositories.*;
 import io.github.saulmmbp.services.IContactService;
 import io.github.saulmmbp.utils.mappers.IContactMapper;
 import lombok.AllArgsConstructor;
@@ -24,60 +19,70 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ContactService implements IContactService {
 
-	private IContactRepository contactRepository;
-	private IContactMapper contactMapper;
-	
-	@Override
-	public Page<ContactResponseDto> getContactsByUserId(String userId, Pageable pageable) {
-		Page<Contact> contacts = contactRepository.findAllByUserId(userId, pageable);
-		return contacts.map(contact -> contactMapper.toResponseDto(contact));
-	}
+    private IContactRepository contactRepository;
+    private IUserRepository userRepository;
+    private IContactMapper contactMapper;
 
-	@Override
-	public ContactResponseDto getContactByIdAndUserId(Long contactId, String userId) {
-		if (!contactRepository.existsById(contactId)) throw new ResourceNotFoundException("Contact", contactId);
-		Contact contact = contactRepository.findByIdAndUserId(contactId, userId).orElseThrow(() -> new ResourceNotBelongsUserException("Contact", contactId, userId));
-		return contactMapper.toResponseDto(contact);
-	}
+    @Override
+    public Page<ContactResponseDto> getContactsByUserId(String userId, Pageable pageable) {
+        Page<Contact> contacts = contactRepository.findAllByUserId(userId, pageable);
+        return contacts.map(contact -> contactMapper.toResponseDto(contact));
+    }
 
-	@Override
-	public ContactResponseDto createContactForUser(String userId, ContactRequestDto contactRequest) {
-		Contact contact = contactMapper.toEntity(contactRequest);
-		log.debug("Adding contact {} for user {}", contact, userId);
-		contact.setUserId(userId);
-		Contact savedContact = contactRepository.save(contact);
-		return contactMapper.toResponseDto(savedContact);
-	}
+    @Override
+    public ContactResponseDto getContactByIdAndUserId(Long contactId, String userId) {
+        if (!contactRepository.existsById(contactId))
+            throw new ResourceNotFoundException("Contact", contactId);
+        Contact contact = contactRepository.findByIdAndUserId(contactId, userId)
+                .orElseThrow(() -> new ResourceNotBelongsUserException("Contact", contactId, userId));
+        return contactMapper.toResponseDto(contact);
+    }
 
-	@Override
-	public ContactResponseDto updateContactForUser(Long contactId, String userId, ContactRequestDto contactRequest) {
-		if (!contactRepository.existsById(contactId)) throw new ResourceNotFoundException("Contact", contactId);
-		Contact contact = contactRepository.findByIdAndUserId(contactId, userId).orElseThrow(() -> new ResourceNotBelongsUserException("Contact", contactId, userId));
-		contact.setIdentifier(contactRequest.identifier());
-		contact.setName(contactRequest.name());
-		contact.setCompany(contactRequest.company());
-		contact.setEmail(contactRequest.email());
-		contact.setPosition(contactRequest.position());
-		contact.setWeb(contactRequest.web());
-		
-		List<Phone> phones = new ArrayList<>();
-		contactRequest.phones().forEach(phoneNumber -> {
-			for (Phone phone : contact.getPhones()) {
-				if (phoneNumber.equals(phone.getPhoneNumber())) phones.add(phone);
-				else phones.add(new Phone(phoneNumber));
-			}
-		});
-		
-		Contact updatedContact = contactRepository.save(contact);
-		
-		return contactMapper.toResponseDto(updatedContact);
-	}
+    @Override
+    public ContactResponseDto createContactForUser(String userId, ContactRequestDto contactRequest) {
+        if (!userRepository.existsById(userId)) throw new ResourceNotFoundException("User", userId);
+        Contact contact = contactMapper.toEntity(contactRequest);
+        log.debug("Adding contact {} for user {}", contact, userId);
+        contact.setUserId(userId);
+        Contact savedContact = contactRepository.save(contact);
+        return contactMapper.toResponseDto(savedContact);
+    }
 
-	@Override
-	public void deleteContactForUser(Long contactId, String userId) {
-		if (!contactRepository.existsById(contactId)) throw new ResourceNotFoundException("Contact", contactId);
-		Contact contact = contactRepository.findByIdAndUserId(contactId, userId).orElseThrow(() -> new ResourceNotBelongsUserException("Contact", contactId, userId));
-		contactRepository.delete(contact);
-	}
-	
+    @Override
+    public ContactResponseDto updateContactForUser(Long contactId, String userId, ContactRequestDto contactRequest) {
+        if (!contactRepository.existsById(contactId))
+            throw new ResourceNotFoundException("Contact", contactId);
+        Contact contact = contactRepository.findByIdAndUserId(contactId, userId)
+                .orElseThrow(() -> new ResourceNotBelongsUserException("Contact", contactId, userId));
+        contact.setIdentifier(contactRequest.identifier());
+        contact.setName(contactRequest.name());
+        contact.setCompany(contactRequest.company());
+        contact.setEmail(contactRequest.email());
+        contact.setPosition(contactRequest.position());
+        contact.setWeb(contactRequest.web());
+
+        List<Phone> phones = new ArrayList<>();
+        contactRequest.phones().forEach(phoneNumber -> {
+            for (Phone phone : contact.getPhones()) {
+                if (phoneNumber.equals(phone.getPhoneNumber()))
+                    phones.add(phone);
+                else
+                    phones.add(new Phone(phoneNumber));
+            }
+        });
+
+        Contact updatedContact = contactRepository.save(contact);
+
+        return contactMapper.toResponseDto(updatedContact);
+    }
+
+    @Override
+    public void deleteContactForUser(Long contactId, String userId) {
+        if (!contactRepository.existsById(contactId))
+            throw new ResourceNotFoundException("Contact", contactId);
+        Contact contact = contactRepository.findByIdAndUserId(contactId, userId)
+                .orElseThrow(() -> new ResourceNotBelongsUserException("Contact", contactId, userId));
+        contactRepository.delete(contact);
+    }
+
 }
--- src/main/java/io/github/saulmmbp/services/impl/VCardService.java
+++ src/main/java/io/github/saulmmbp/services/impl/VCardService.java
@@ -1,22 +1,15 @@
 package io.github.saulmmbp.services.impl;
 
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.Resource;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.core.io.*;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 
-import io.github.saulmmbp.dtos.VCardRequestDto;
-import io.github.saulmmbp.dtos.VCardResponseDto;
-import io.github.saulmmbp.entities.Contact;
-import io.github.saulmmbp.entities.VCard;
-import io.github.saulmmbp.exceptions.ResourceNotBelongsUserException;
-import io.github.saulmmbp.exceptions.ResourceNotFoundException;
-import io.github.saulmmbp.repositories.IContactRepository;
-import io.github.saulmmbp.repositories.IVCardRepository;
+import io.github.saulmmbp.dtos.*;
+import io.github.saulmmbp.entities.*;
+import io.github.saulmmbp.exceptions.*;
+import io.github.saulmmbp.repositories.*;
 import io.github.saulmmbp.services.IVCardService;
-import io.github.saulmmbp.utils.QRCodeUtils;
-import io.github.saulmmbp.utils.VCardUtils;
+import io.github.saulmmbp.utils.*;
 import io.github.saulmmbp.utils.mappers.IVCardMapper;
 import lombok.AllArgsConstructor;
 
@@ -24,79 +17,91 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class VCardService implements IVCardService {
 
-	private IVCardRepository vcardRepository;
-	private IContactRepository contactRepository;
-	private IVCardMapper vcardMapper;
-	private QRCodeUtils qrCodeUtils;
-	
-	@Override
-	public VCardResponseDto createVCardForUser(String userId, VCardRequestDto vcardRequest) {
-		VCard vcard = vcardMapper.toEntity(vcardRequest);
-		vcard.setUserId(userId);
-		
-		if (!contactRepository.existsById(vcardRequest.contactId())) throw new ResourceNotFoundException("Contact", vcardRequest.contactId());
-		Contact contact = contactRepository.findByIdAndUserId(vcardRequest.contactId(), userId)
-				.orElseThrow(() -> new ResourceNotBelongsUserException("Contact", vcardRequest.contactId(), userId));
-		vcard.setContact(contact);
-		
-		String vcardStr = VCardUtils.convertToPlainTextVCard(vcard);
-		byte[] qr = qrCodeUtils.generateQRCode(vcardStr);
-		vcard.setQr(qr);
-		
-		VCard savedVCard = vcardRepository.save(vcard);
-		return vcardMapper.toResponseDto(savedVCard);
-	}
+    private IVCardRepository vcardRepository;
+    private IContactRepository contactRepository;
+    private IUserRepository userRepository;
+    private IVCardMapper vcardMapper;
+    private QRCodeUtils qrCodeUtils;
 
-	@Override
-	public Page<VCardResponseDto> getVCardsByUserId(String userId, Pageable pageable) {
-		Page<VCard> vcards = vcardRepository.findByUserId(userId, pageable);
-		Page<VCardResponseDto> vcardsResponse = vcards.map(vcard -> vcardMapper.toResponseDto(vcard));
-		return vcardsResponse;
-	}
+    @Override
+    public VCardResponseDto createVCardForUser(String userId, VCardRequestDto vcardRequest) {
+        if (!userRepository.existsById(userId)) throw new ResourceNotFoundException("User", userId);
+        VCard vcard = vcardMapper.toEntity(vcardRequest);
+        vcard.setUserId(userId);
 
-	@Override
-	public VCardResponseDto getVCardByIdAndUserId(Long vcardId, String userId) {
-		if (!vcardRepository.existsById(vcardId)) throw new ResourceNotFoundException("VCard", vcardId);
-		VCard vcard = vcardRepository.findByIdAndUserId(vcardId, userId).orElseThrow(() -> new ResourceNotBelongsUserException("VCard", vcardId, userId));
-		return vcardMapper.toResponseDto(vcard);
-	}
+        if (!contactRepository.existsById(vcardRequest.contactId()))
+            throw new ResourceNotFoundException("Contact", vcardRequest.contactId());
+        Contact contact = contactRepository.findByIdAndUserId(vcardRequest.contactId(), userId)
+                .orElseThrow(() -> new ResourceNotBelongsUserException("Contact", vcardRequest.contactId(), userId));
+        vcard.setContact(contact);
 
-	@Override
-	public VCardResponseDto updateVCardForUser(Long vcardId, String userId, VCardRequestDto vcardRequest) {
-		if (!vcardRepository.existsById(vcardId)) throw new ResourceNotFoundException("VCard", vcardId);
-		VCard vcard = vcardRepository.findByIdAndUserId(vcardId, userId).orElseThrow(() -> new ResourceNotBelongsUserException("VCard", vcardId, userId));
-		vcard.setColor(vcardRequest.color());
-		vcard.setName(vcardRequest.name());
-		
-		if (vcardRequest.contactId() != vcard.getContact().getId()) {
-			if (!contactRepository.existsById(vcardRequest.contactId())) throw new ResourceNotFoundException("Contact", vcardRequest.contactId());
-			Contact newContact = contactRepository.findByIdAndUserId(vcardRequest.contactId(), userId)
-					.orElseThrow(() -> new ResourceNotBelongsUserException("Contact", vcardRequest.contactId(), userId));
-			vcard.setContact(newContact);
-			
-			String vcardStr = VCardUtils.convertToPlainTextVCard(vcard);
-			byte[] qr = qrCodeUtils.generateQRCode(vcardStr);
-			vcard.setQr(qr);
-		}
-		
-		VCard savedVCard = vcardRepository.save(vcard);
-		return vcardMapper.toResponseDto(savedVCard);
-	}
+        String vcardStr = VCardUtils.convertToPlainTextVCard(vcard);
+        byte[] qr = qrCodeUtils.generateQRCode(vcardStr);
+        vcard.setQr(qr);
 
-	@Override
-	public void deleteVCardForUser(Long vcardId, String userId) {
-		if (!vcardRepository.existsById(vcardId)) throw new ResourceNotFoundException("VCard", vcardId);
-		VCard vcard = vcardRepository.findByIdAndUserId(vcardId, userId).orElseThrow(() -> new ResourceNotBelongsUserException("VCard", vcardId, userId));
-		vcardRepository.delete(vcard);
-	}
+        VCard savedVCard = vcardRepository.save(vcard);
+        return vcardMapper.toResponseDto(savedVCard);
+    }
 
-	@Override
-	public Resource downloadQRImage(Long vcardId, String userId) {
-		if (!vcardRepository.existsById(vcardId)) throw new ResourceNotFoundException("VCard", vcardId);
-		VCard vcard = vcardRepository.findByIdAndUserId(vcardId, userId).orElseThrow(() -> new ResourceNotBelongsUserException("VCard", vcardId, userId));
-		byte[] qr = vcard.getQr();
-		ByteArrayResource resource = new ByteArrayResource(qr);
-		return resource;
-	}
+    @Override
+    public Page<VCardResponseDto> getVCardsByUserId(String userId, Pageable pageable) {
+        Page<VCard> vcards = vcardRepository.findByUserId(userId, pageable);
+        Page<VCardResponseDto> vcardsResponse = vcards.map(vcard -> vcardMapper.toResponseDto(vcard));
+        return vcardsResponse;
+    }
+
+    @Override
+    public VCardResponseDto getVCardByIdAndUserId(Long vcardId, String userId) {
+        if (!vcardRepository.existsById(vcardId))
+            throw new ResourceNotFoundException("VCard", vcardId);
+        VCard vcard = vcardRepository.findByIdAndUserId(vcardId, userId)
+                .orElseThrow(() -> new ResourceNotBelongsUserException("VCard", vcardId, userId));
+        return vcardMapper.toResponseDto(vcard);
+    }
+
+    @Override
+    public VCardResponseDto updateVCardForUser(Long vcardId, String userId, VCardRequestDto vcardRequest) {
+        if (!vcardRepository.existsById(vcardId))
+            throw new ResourceNotFoundException("VCard", vcardId);
+        VCard vcard = vcardRepository.findByIdAndUserId(vcardId, userId)
+                .orElseThrow(() -> new ResourceNotBelongsUserException("VCard", vcardId, userId));
+        vcard.setColor(vcardRequest.color());
+        vcard.setName(vcardRequest.name());
+
+        if (vcardRequest.contactId() != vcard.getContact().getId()) {
+            if (!contactRepository.existsById(vcardRequest.contactId()))
+                throw new ResourceNotFoundException("Contact", vcardRequest.contactId());
+            Contact newContact = contactRepository.findByIdAndUserId(vcardRequest.contactId(), userId).orElseThrow(
+                    () -> new ResourceNotBelongsUserException("Contact", vcardRequest.contactId(), userId));
+            vcard.setContact(newContact);
+
+            String vcardStr = VCardUtils.convertToPlainTextVCard(vcard);
+            byte[] qr = qrCodeUtils.generateQRCode(vcardStr);
+            vcard.setQr(qr);
+        }
+
+        VCard savedVCard = vcardRepository.save(vcard);
+        return vcardMapper.toResponseDto(savedVCard);
+    }
+
+    @Override
+    public void deleteVCardForUser(Long vcardId, String userId) {
+        if (!vcardRepository.existsById(vcardId))
+            throw new ResourceNotFoundException("VCard", vcardId);
+        VCard vcard = vcardRepository.findByIdAndUserId(vcardId, userId)
+                .orElseThrow(() -> new ResourceNotBelongsUserException("VCard", vcardId, userId));
+        vcardRepository.delete(vcard);
+    }
+
+    @Override
+    public Resource downloadQRImage(Long vcardId, String userId) {
+        if (!vcardRepository.existsById(vcardId))
+            throw new ResourceNotFoundException("VCard", vcardId);
+        VCard vcard = vcardRepository.findByIdAndUserId(vcardId, userId)
+                .orElseThrow(() -> new ResourceNotBelongsUserException("VCard", vcardId, userId));
+        byte[] qr = vcard.getQr();
+        ByteArrayResource resource = new ByteArrayResource(qr);
+        return resource;
+    }
 
 }
