0ea74f59ed273f1f1ec7ad9a3fabf199b670e96c
Saul Malagon Martinez <sark_blues_friki95@hotmail.com>
2025-06-04 01:17:17 -0600

    Se agregó manejo de excepciones por validación

--- src/main/java/io/github/saulmmbp/dtos/ErrorDetailsDto.java
+++ src/main/java/io/github/saulmmbp/dtos/ErrorDetailsDto.java
@@ -1,7 +1,11 @@
 package io.github.saulmmbp.dtos;
 
-import java.util.Date;
+import java.util.*;
 
-public record ErrorDetailsDto(Date timestamp, String message, String details) {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
+@JsonInclude(Include.NON_NULL)
+public record ErrorDetailsDto(Date timestamp, String message, Map<String, String> messages, String details) {
+    
 }
--- src/main/java/io/github/saulmmbp/exceptions/GlobalExceptionHandler.java
+++ src/main/java/io/github/saulmmbp/exceptions/GlobalExceptionHandler.java
@@ -1,55 +1,70 @@
 package io.github.saulmmbp.exceptions;
 
-import java.util.Date;
+import java.util.*;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.http.*;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
 
 import io.github.saulmmbp.dtos.ErrorDetailsDto;
+import lombok.extern.slf4j.Slf4j;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ErrorDetailsDto> runtimeExceptionHandler(RuntimeException exception, WebRequest request) {
-        ErrorDetailsDto errorDetails = new ErrorDetailsDto(new Date(), exception.toString(),
+        ErrorDetailsDto errorDetails = new ErrorDetailsDto(new Date(), exception.toString(), null,
                 request.getDescription(false));
+        log.error(exception.getMessage());
         return ResponseEntity.internalServerError().body(errorDetails);
     }
 
     @ExceptionHandler(ApplicationException.class)
     public ResponseEntity<ErrorDetailsDto> applicationExceptionHandler(ApplicationException exception,
             WebRequest request) {
-        ErrorDetailsDto errorDetails = new ErrorDetailsDto(new Date(), exception.getMessage(),
+        ErrorDetailsDto errorDetails = new ErrorDetailsDto(new Date(), exception.getMessage(), null,
                 request.getDescription(false));
+        log.error(exception.getMessage());
         return ResponseEntity.internalServerError().body(errorDetails);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorDetailsDto> resourceNotFoundExceptionHandler(ResourceNotFoundException exception,
             WebRequest request) {
-        ErrorDetailsDto errorDetails = new ErrorDetailsDto(new Date(), exception.getMessage(),
+        ErrorDetailsDto errorDetails = new ErrorDetailsDto(new Date(), exception.getMessage(), null,
                 request.getDescription(false));
+        log.info(exception.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorDetails);
     }
 
     @ExceptionHandler(ResourceNotBelongsUserException.class)
     public ResponseEntity<ErrorDetailsDto> resourceNotBelongsUserExceptionHandler(
             ResourceNotBelongsUserException exception, WebRequest request) {
-        ErrorDetailsDto errorDetails = new ErrorDetailsDto(new Date(), exception.getMessage(),
+        ErrorDetailsDto errorDetails = new ErrorDetailsDto(new Date(), exception.getMessage(), null,
                 request.getDescription(false));
+        log.info(exception.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorDetails);
     }
     
     @ExceptionHandler(ResourceAlreadyExistsException.class)
     public ResponseEntity<ErrorDetailsDto> resourceAlreadyExistsExceptionHandler(
             ResourceNotBelongsUserException exception, WebRequest request) {
-        ErrorDetailsDto errorDetails = new ErrorDetailsDto(new Date(), exception.getMessage(),
+        ErrorDetailsDto errorDetails = new ErrorDetailsDto(new Date(), exception.getMessage(), null,
                 request.getDescription(false));
+        log.info(exception.getMessage());
         return ResponseEntity.status(HttpStatus.CONFLICT).body(errorDetails);
+    }
+    
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDetailsDto> resourceConstraintValidationExceptionHandler(MethodArgumentNotValidException exception, WebRequest request) {
+        Map<String, String> messages = new HashMap<>();
+        exception.getBindingResult().getFieldErrors().forEach(fieldError -> messages.put(fieldError.getField(), fieldError.getDefaultMessage()));
+        ErrorDetailsDto errorDetails = new ErrorDetailsDto(new Date(), null, messages, request.getDescription(false));
+        log.info(exception.getMessage());
+        return ResponseEntity.badRequest().body(errorDetails);
     }
     
 }
